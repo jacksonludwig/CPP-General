fe572b33f9af7f44d855094519fba056274d98e1
Jackson <jacksonludwig74@yahoo.com>
2020-01-24 23:36:18 -0500

    added start project

---
+++ HelloWorld/HelloWorld/Main2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+double square(double x);
+void print_square(double);
+
+int main() {
+	print_square(10);
+	return 0;
+}
+
+double square(double x) {
+	return x * x;
+}
+
+void print_square(double x) {
+	cout << "the square of " << x << " is " << square(x) << "\n";
+}
+
